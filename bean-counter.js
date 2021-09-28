51785daa4a8c1cd8712f1f70c40729fab72fb0ac
sausagem8y <theworldstrash@gmail.com>
2021-09-28 13:38:55 -0600

    Update bean-counter.js
    tried adding button interaction

--- bean-counter.js
+++ bean-counter.js
@@ -1,3 +1,4 @@
+const btn = document.getElementById("button");
 const cupOf = InputEvent;
 const cup = 136;
 const gram = InputEvent;
@@ -7,3 +8,5 @@ function conversion(cup, cupOf, gram) {
   let cupConvert = gram / 136;
   let measureAlt = cupConvert * 150;
 }
+
+btn.addEventListener(onclick, conversion);
